41d9015ed1288508e56088e16774cffce5e18075
Jason Polete <jpolete@poleteweb.com>
2015-04-16 16:30:05 -0400

    added some boilerplate js

---
+++ js/_start.js
@@ -0,0 +1,6 @@
+//
+// Initialize JavaScript that should run on every page here.
+//
+( function( window, app, undefined ) {
+  //app.doSomething();
+})( this, __app__ );
--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
-window.myApp = {};
+// Exclude Shoestring/jQuery if we're only using it for document ready.
+// If that's the case, just move script element to bottom of the page.
+// @disabled-codekit-prepend "lib/shoestring.min.js";
+// @disabled-codekit-prepend "lib/jquery.min.js";
+
+// Global variable to which to attach all your code.
+// Attach all functions and variables to this so we don't pollute the global 
+// context.
+var __app__ = {};
+
+// Utilities
+// @codekit-append "_util.js";
+
+// Named breakpoints (these should really match named Sass breakpoints)
+// @codekit-append "_breakpoints.js";
+
+// Run / initialize code defined in other modules
+// @codekit-append "_start.js";
