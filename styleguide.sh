c0d818b893566b32daa363b74822b1eff77b682b
Jason Polete <jason@jpolete.me>
2016-01-13 17:23:09 -0500

    Process sass in style guide script
    Codekit will auto-process sass on update, but added sass commands
    to the styleguide script just to ensure they're processed for
    brand new projects.

--- styleguide.sh
+++ styleguide.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# Process style guide sass
+sass -t compressed styleguide-tmpl/public/sbstyleguide.scss styleguide-tmpl/public/sbstyleguide.css
+
+# Process site sass
+sass -t compressed components/style.scss style.css
+
+# Run kss-node to generate style guide
 kss-node components styleguide --template styleguide-tmpl --helpers styleguide-tmpl/helpers --css public/style.css
 cp style.css styleguide/public/style.css
