048b257ee92abb014d2d238450ece39ff8150835
Jason Polete <jason@jpolete.me>
2016-08-19 10:04:13 -0400

    Remove git commands from Fractal build script

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Get on master
-git checkout develop
-
-# Make sure we're up to date
-git pull
-git push
-
 # Compile SASS
-sass -t compressed style.scss ../public/style.css
+(cd componentlib/sass && sass -t compressed style.scss ../public/style.css)
 
 # Build static Fractal Web UI
-(cd componentlib && fractal build && cd ..)
+(cd componentlib && fractal build)
 
 # Copy to temp, then switch to gh-pages???
-git checkout gh-pages
-cp -R componentlib/build/. ./
+# git checkout gh-pages
+# cp -R componentlib/build/. ./
+# rm -r componentlib/
